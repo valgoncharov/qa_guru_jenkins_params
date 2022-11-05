afd4467f4f593143a6bdef24e94efa5bb9476541
valgoncharov <goncharovvalentyn@gmail.com>
2022-11-05 17:36:13 +0300

    Homework - jenkins with params. Add folder build. Add Tags. Off local web driver. Add remote url

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -7,6 +7,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import tests.properties.Property;
 
 public class TestBase {
     @BeforeAll
@@ -19,10 +20,17 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = Property.browser();
+        Configuration.browserSize = Property.browserSize();
+        Configuration.browserVersion = Property.browserVersion();
+        if (!Property.remoteUrl().equals("")){
+            Configuration.remote = Property.remoteUrl();
+        }
+
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         //System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
     }
+
 
     @AfterEach
     void addAttachments(){
